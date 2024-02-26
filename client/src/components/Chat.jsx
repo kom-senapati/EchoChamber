872185631c376afa3a2e69f0f770deda9bec8a7f
kom-senapati <komnoob123@gmail.com>
2024-02-26 21:39:25 +0530

    chat-bubble-update

--- client/src/components/Chat.jsx
+++ client/src/components/Chat.jsx
@@ -1,20 +1,16 @@
 import React, { useState, useEffect, useRef, useContext } from "react";
 import InputEmoji from "react-input-emoji";
 import { IoSend } from "react-icons/io5";
-import io from "socket.io-client"
+import io from "socket.io-client";
 import { useParams } from "react-router-dom";
 import { userInfo } from "../App";
 import axios from "axios";
 
-
 const ENDPOINT = "http://localhost:3000";
 let socket, selectedChatCompare;
 
-
-
 const Chat = () => {
-
-  const apiEndpoint = 'http://localhost:3000/'
+  const apiEndpoint = "http://localhost:3000/";
   const { groupId } = useParams();
   const { currentUser, setCurrentUser } = useContext(userInfo);
   const [message, setMessage] = useState("");
@@ -22,45 +18,37 @@ const Chat = () => {
   const chatContainerRef = useRef(null);
   const [selectedChat, setSelectedChat] = useState();
 
-
   useEffect(() => {
     fetchMessages();
-    socket = io(ENDPOINT)
-  }, [])
-
-
+    socket = io(ENDPOINT);
+  }, []);
 
   useEffect(() => {
-
-    socket.on('new-message', (incoming_message) => {
-      console.log('incoming_message');
-      setChats([...chats, incoming_message])
-    })
+    socket.on("new-message", (incoming_message) => {
+      console.log("incoming_message");
+      setChats([...chats, incoming_message]);
+    });
     // Scroll chat container to the bottom
     chatContainerRef.current.scrollTop = chatContainerRef.current.scrollHeight;
-
-  })
-
-
-
+  });
 
   const fetchMessages = async () => {
     try {
       const resp = await axios.get(`${apiEndpoint}message/getmessage`, {
         params: {
-          chatId: groupId
-        }
-      })
+          chatId: groupId,
+        },
+      });
       if (resp.status == 200) {
-/*         console.log(resp.data[0]);
- */        setChats(resp.data)
-        setSelectedChat(resp.data[0].chat._id)
-        socket.emit('join-chat', (groupId))
+        /*         console.log(resp.data[0]);
+         */ setChats(resp.data);
+        setSelectedChat(resp.data[0].chat._id);
+        socket.emit("join-chat", groupId);
       }
     } catch (error) {
       console.log(error);
     }
-  }
+  };
 
   const handleMessageChange = (e) => {
     setMessage(e.target.value);
@@ -68,24 +56,21 @@ const Chat = () => {
     e.target.style.height = `${e.target.scrollHeight}px`;
   };
 
-
-
   const handleSendMessage = async () => {
     try {
       const resp = await axios.post(`${apiEndpoint}message/sendmessage`, {
         userId: currentUser._id,
         content: message,
-        chatId: selectedChat
-      })
+        chatId: selectedChat,
+      });
       if (resp.status == 200) {
         console.log(resp.data);
-        setChats([...chats, resp.data])
-        socket.emit('new-message', (resp.data))
+        setChats([...chats, resp.data]);
+        socket.emit("new-message", resp.data);
       }
     } catch (error) {
       console.log(error);
     }
-
   };
 
   const handleKeyDown = (e) => {
@@ -95,7 +80,6 @@ const Chat = () => {
     }
   };
 
-
   return (
     <>
       <input onChange={(e) => setCurrentUser(e.target.value)} />
@@ -103,22 +87,28 @@ const Chat = () => {
         <section className="flex  h-[80%] w-[80%] border border-gray-700 rounded-md">
           <aside className="w-full flex-1 border-r border-gray-700 h-full ">
             {/* Groups the current user currenly in */}
-
           </aside>
           <div className="flex flex-col flex-[3] w-full border border-none shadow-md rounded px-4 py-2">
             <div ref={chatContainerRef} className="overflow-y-auto flex-grow">
               {chats.map((chat, index) => (
                 <>
-
-                  {chat.sender === currentUser._id ? (
+                  {chat.sender._id === currentUser._id ? (
                     <>
                       <div key={index} className="chat chat-end">
-                        <div className="chat-bubble">{chat.content}</div>
+                        <div className="chat-header">
+                          {chat.sender.username}
+                        </div>
+                        <div className="chat-bubble chat-bubble-primary">
+                          {chat.content}
+                        </div>
                       </div>
                     </>
                   ) : (
                     <>
                       <div key={index} className="chat chat-start">
+                        <div className="chat-header">
+                          {chat.sender.username}
+                        </div>
                         <div className="chat-bubble">{chat.content}</div>
                       </div>
                     </>
@@ -136,13 +126,16 @@ const Chat = () => {
                 borderRadius={10}
                 fontSize={20}
               />
-              <button onClick={handleSendMessage} onChange={handleMessageChange} className="btn btn-square">
+              <button
+                onClick={handleSendMessage}
+                onChange={handleMessageChange}
+                className="btn btn-square"
+              >
                 <IoSend size={25} />
               </button>
             </div>
           </div>
         </section>
-
       </main>
     </>
   );
