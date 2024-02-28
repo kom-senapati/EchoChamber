00ab2edca5586915489a5748dde983bf6c6471ae
Sagar Singh <sagar.01ko@gmail.com>
2024-02-28 15:30:20 +0530

    Add: Chnaged the UI of Home page

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,6 +3,7 @@ import Register from "./components/Register";
 import Login from "./components/Login";
 import Home from "./components/Home";
 import Chat from "./components/Chat";
+import Hero from "./components/Hero";
 import { createContext, useState } from "react";
 
 export const userInfo = createContext(null);
@@ -19,7 +20,8 @@ function App() {
     >
       <Router>
         <Routes>
-          <Route path="/" element={<Register />} />
+          <Route path="/" element={<Hero />} />
+          <Route path="/register" element={<Register />} />
           <Route path="/login" element={<Login />} />
           <Route path="/home" element={<Home />} />
           <Route path="/home/group/:groupId" element={<Chat />} />
